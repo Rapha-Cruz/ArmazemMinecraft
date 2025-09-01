dfd627a07429d888ca633a95263d12a167ba40b9
Raphael Cruz <raphajcruz@hotmail.com>
2025-09-01 19:37:05 -0300

    teste local

--- conexao.js
+++ conexao.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    // user: process.env.DB_USER,
-    // host: process.env.DB_HOST,
-    // database: process.env.DB_NAME,
-    // password: process.env.DB_PASSWORD,
-    // port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
 
-    connectionString: process.env.DATABASE_URL,
+    // connectionString: process.env.DATABASE_URL,
     //precisa para o supabase
     ssl: {
         rejectUnauthorized: false // necessário para Supabase
